c0de4a6032e2ddf745c28be3304df5645915f74c
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-07-18 11:28:39 +0200

    Finished with Travel Journal Project

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 import Travel from './Travel'
+import travels from '../data'
 
 export default function MainContent() {
-  return (
-    <Travel />
-  )
+    const travelJournals = travels.map((travel, index) => (
+        <>
+            <Travel
+                key={index}
+                title={travel.title}
+                location={travel.location}
+                googleMapsUrl={travel.googleMapsUrl}
+                startDate={travel.startDate}
+                endDate={travel.endDate}
+                description={travel.description}
+                imageUrl={travel.imageUrl}
+            />
+            <div className='line'></div>
+        </>
+    ));
+
+    return (
+        <div className='travels'>
+            {travelJournals}
+        </div>
+    );
 }
--- src/components/Travel.jsx
+++ src/components/Travel.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
-import cardImg from "../../public/images/mount-fuji.jpg"
 import googleMapImg from "../../public/images/google-maps.png"
 
-export default function Travel() {
+export default function Travel(props) {
+    const {title, location, googleMapsUrl, startDate, endDate, description, imageUrl} = props
   return (
     <div className="card">
-        <img src={cardImg} alt="Card Main Image" className='card--image'/>
+        <img src={imageUrl} alt="Card Main Image" className='card--image'/>
         <div className="description">
             <div className='directions'>
                 <img src={googleMapImg} alt="Google Map Icon" />
-                <h4>JAPAN</h4>
-                <a href="#">View on Google Maps</a>
+                <h4>{location.toUpperCase()}</h4>
+                <a href={googleMapsUrl}>View on Google Maps</a>
             </div>
-            <h1>Mount Fuji</h1>
-            <h3>12 Jan, 2021 - 24 Jan 2021</h3>
-            <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+            <h1>{title}</h1>
+            <h3>{startDate} - {endDate}</h3>
+            <p>{description}</p>
         </div>
     </div>
   )
